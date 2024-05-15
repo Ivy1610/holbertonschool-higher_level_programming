5123e5fcbbf919fb1478c883caa6dc6c6374f625
Ivy1610 <8682@holbertonstudent.com>
2024-05-15 12:21:45 +0200

    fix prblem pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -16,13 +16,14 @@ def value(roman):
         return 1000
     return 0
 
+
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
 
     if not isinstance(roman_string, str):
         return 0
-    
+
     result = 0
     i = 0
 
