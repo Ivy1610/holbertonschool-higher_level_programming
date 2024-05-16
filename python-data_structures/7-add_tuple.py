64a24db04a4fcda6a9443dcf79a967564b58b32f
Ivy1610 <8682@holbertonstudent.com>
2024-05-16 03:33:00 +0200

    ajout des parenthese au return

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    
+
     tuple_a_lenght = len(tuple_a)
     tuple_b_lenth = len(tuple_b)
 
@@ -23,7 +23,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     else:
         b1 = tuple_b[0]
         b2 = tuple_b[1]
-    
+
     new_tuple = (a1 + b1, a2 + b2)
 
-    return new_tuple
+    return (new_tuple)
