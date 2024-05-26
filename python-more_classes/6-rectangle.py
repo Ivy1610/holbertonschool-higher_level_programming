289ee1fae8e975fbf8318685802043067d5a2705
Ivy1610 <8682@holbertonstudent.com>
2024-05-26 17:42:54 +0200

    modifier le nom du numero d'instance

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        type(self).number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -75,4 +75,4 @@ class Rectangle:
     def __del__(self):
         """Delete object/instance"""
         print("Bye retcangle...")
-        type(self).number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
