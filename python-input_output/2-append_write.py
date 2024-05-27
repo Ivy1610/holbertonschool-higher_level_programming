59c5191f4f9e050fe25e7ff0fc9d0f4bb14d79d1
Ivy1610 <8682@holbertonstudent.com>
2024-05-27 17:57:01 +0200

    modifier les conditions

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,7 +5,7 @@
 def append_write(filename="", text=""):
     """appends a string at the end of a text file and returns the number
     of characters added
-    
+
     Agrs:
         filename (str, optional): name of the file
         text (str, optional): string of text to write to file
@@ -13,6 +13,8 @@ def append_write(filename="", text=""):
     returns:
         int: number of characters appended to file
     """
+    count = 0
     with open(filename, 'a', encoding="utf-8") as file:
         """This method returns the number of characters appended to a file"""
-        return file.write(text)
+        count = file.write(text)
+    return count
