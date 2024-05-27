0409fdc87aa0a5f8af480a5149dcd815aa055e0c
Ivy1610 <8682@holbertonstudent.com>
2024-05-27 10:17:23 +0200

    ajouter des commentaires

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""Module defines the read_file function"""
+
+
 def read_file(filename=""):
-    """
-    Defines the read_file function
+    """Reads a file and prints to stdout
+
+    Args:
+        filename (str, optional): name of file to read
     """
     with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
