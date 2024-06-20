c4f7413b4e2fea1c944d24201ae5e6af7b30a9a7
Ivy1610 <8682@holbertonstudent.com>
2024-06-20 15:03:56 +0200

    First table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,11 @@
+-- create database hbtn_0c_0
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+-- First table
+USE `hbtn_0c_0`;
+
+-- create the table
+CREATE TABLE IF NOT EXISTS `first_table` (
+	`id`INT,
+	`name`VARCHAR(256)
+);
