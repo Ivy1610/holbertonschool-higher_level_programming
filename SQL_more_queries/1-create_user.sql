20519ee31fc4e0099382b11b25c7ff5e64785e54
Ivy1610 <8682@holbertonstudent.com>
2024-06-21 09:39:14 +0200

    fix the probleme

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,8 @@
 -- creates the MySQL server user user_0d_1
-CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- all privileges for user_0d_1
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- flush privileges
+FLUSH PRIVILEGES;
