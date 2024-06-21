c72db7d847999cf698334797050792576f9609a5
Ivy1610 <8682@holbertonstudent.com>
2024-06-21 09:27:17 +0200

    modification sur le fichier

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
 -- creates the MySQL server user user_0d_1
 CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- all privileges to user_0d_1
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
