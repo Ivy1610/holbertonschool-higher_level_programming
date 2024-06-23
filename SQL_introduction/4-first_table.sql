8cdc91f33258d70f0293df89bd1a8df15713f213
Ivy1610 <8682@holbertonstudent.com>
2024-06-23 14:17:25 +0200

    enlever les commentaires

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,10 +1,6 @@
 -- create database hbtn_0c_0
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
--- First table
 USE `hbtn_0c_0`;
-
--- create the table
 CREATE TABLE IF NOT EXISTS `first_table` (
 	`id`INT,
 	`name`VARCHAR(256)
