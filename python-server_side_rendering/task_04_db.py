755b781d2dda794900d44cb95016c892d7aa6bce
Ivy1610 <8682@holbertonschool.com>
2024-07-16 23:15:53 +0200

    Extending Dynamic Data Display to Include SQLite in Flask

--- python-server_side_rendering/task_04_db.py
+++ python-server_side_rendering/task_04_db.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import json
 import csv
+import sqlite3
 
 app = Flask(__name__)
 
@@ -18,18 +19,32 @@ def read_csv(file_path):
             products.append(row)
     return products
 
+def read_sqlite(db_path):
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute('SELECT id, name, category, price FROM Products')
+        products = cursor.fetchall()
+        conn.close()
+        return [{'id': row[0], 'name': row[1], 'category': row[2], 'price': row[3]} for row in products]
+    except sqlite3.Error as e:
+        print(f"Error connecting to database: {e}")
+        return []
+
 @app.route('/products')
 def products():
     source = request.agrs.get('source')
     product_id = request.agrs.get('id', type=int)
 
-    if source not in ['json', 'csv']:
+    if source not in ['json', 'csv', 'sql']:
         return render_template('product_display.html', error='Wrong source')
     
     if source == 'json':
         products = read_json('products.json')
     elif source == 'csv':
         products = read_csv('products.csv')
+    elif source == 'sql':
+        products = read_sqlite('products.db')
 
     if product_id:
         products = [product for product in products if product['id'] == product_id]
