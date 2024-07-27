6ce4af5f251b0cbf886d22d6001c63b9bc4dee03
Ivy1610 <8682@holbertonschool.com>
2024-07-27 14:56:59 +0200

    . Geometry module

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    File class for BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+        class based on geometry.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
