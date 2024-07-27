51ea693556debbce8681c016e0703e028b18719b
Ivy1610 <8682@holbertonschool.com>
2024-07-27 15:06:42 +0200

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    File class for BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+        class based on geometry.
+    """
+    def aera(self):
+        """
+            method that raises an Exception with the message
+        """
+        raise Exception("area() is not implemented")
