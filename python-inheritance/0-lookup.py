65ea5a059a52243a9d5268a0a509ce8048dcfc57
Ivy1610 <8682@holbertonschool.com>
2024-07-27 14:05:24 +0200

    ajouter des commentaires

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+"""
+    function that returns the list of available attributes
+    and methods of an object
+"""
+
+
 def lookup(obj):
     """
-    REturns the list of available attributes and methods of an object
+        function that returns the list of available attributes
+        and methods of an object
+
+    Args:
+        obj: instance of a class
+
+    return:
+        List of attributes
     """
+
     return dir(obj)
